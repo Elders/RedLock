0e47c2de1609ad62cd44ee086e8908e0c624c773
ethno2405 <k.tenekedzhiev@gmail.com>
2016-01-07 10:03:08 +0200

    Removed unused usings.

--- src/RedLock/RedisLockManager.cs
+++ src/RedLock/RedisLockManager.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using RedLock.Logging;
