88228dc32938b2c7b0a8891ca5668b0b94d3a893
mynkow <mynkow@gmail.com>
2023-01-05 16:29:30 +0200

    major: Adds async support

--- src/RedLock/RedisLockManager.cs
+++ src/RedLock/RedisLockManager.cs
@@ -6,51 +6,36 @@ using StackExchange.Redis;
 
 namespace Elders.RedLock
 {
-    public class RedisLockManager : IRedisLockManager
+    public class RedisLockManager : IRedisLockManager, IDisposable
     {
-        private ILogger<RedisLockManager> log;
+        private ILogger<RedisLockManager> logger;
 
         private RedLockOptions options;
 
         private bool isDisposed = false;
 
-        private ConnectionMultiplexer connection;
+        private ConnectionMultiplexer connectionDoNotUse;
 
         public RedisLockManager(IOptionsMonitor<RedLockOptions> options, ILogger<RedisLockManager> logger)
         {
             this.options = options.CurrentValue;
-            log = logger;
-            var configurationOptions = ConfigurationOptions.Parse(options.CurrentValue.ConnectionString);
-            connection = ConnectionMultiplexer.Connect(configurationOptions);
+            this.logger = logger;
         }
 
         public Task<bool> LockAsync(string resource, TimeSpan ttl)
         {
-            return RetryAsync(async () => await AcquireLock(resource, ttl), options.LockRetryCount, options.LockRetryDelay);
+            return RetryAsync(async () => await AcquireLockAsync(resource, ttl), options.LockRetryCount, options.LockRetryDelay);
         }
 
         public Task UnlockAsync(string resource)
         {
-            return UnlockInstance(resource);
+            return UnlockInstanceAsync(resource);
         }
 
         public async Task<bool> IsLockedAsync(string resource)
         {
-            if (connection.IsConnected == false)
-            {
-                log.LogWarning($"Unreachable endpoint '{connection.ClientName}'.");
-                return false;
-            }
-
-            try
-            {
-                return await connection.GetDatabase().KeyExistsAsync(resource, CommandFlags.DemandMaster).ConfigureAwait(false);
-            }
-            catch (Exception ex)
-            {
-                log.LogWarning(ex, $"Unreachable endpoint '{connection.ClientName}'.");
-                return false;
-            }
+            return await ExecuteAsync(async connection =>
+                        await connection.GetDatabase().KeyExistsAsync(resource, CommandFlags.DemandMaster).ConfigureAwait(false)).ConfigureAwait(false);
         }
 
         public void Dispose()
@@ -64,20 +49,20 @@ namespace Elders.RedLock
 
             if (disposing)
             {
-                if (connection != null)
+                if (connectionDoNotUse != null)
                 {
-                    connection.Dispose();
+                    connectionDoNotUse.Dispose();
                 }
 
                 isDisposed = true;
             }
         }
 
-        private async Task<bool> AcquireLock(string resource, TimeSpan ttl)
+        private async Task<bool> AcquireLockAsync(string resource, TimeSpan ttl)
         {
             var startTime = DateTime.Now;
 
-            if (await LockInstance(resource, ttl).ConfigureAwait(false) == false)
+            if (await LockInstanceAsync(resource, ttl).ConfigureAwait(false) == false)
             {
                 return false;
             }
@@ -86,73 +71,94 @@ namespace Elders.RedLock
             var validityTime = ttl - (DateTime.Now - startTime) - new TimeSpan(0, 0, 0, 0, drift);
 
             if (validityTime.TotalMilliseconds > 0)
-            {
                 return true;
-            }
 
-            await UnlockInstance(resource).ConfigureAwait(false);
+            await UnlockInstanceAsync(resource).ConfigureAwait(false);
+
+            logger.LogWarning($"The specified TTL for the resource '{resource}' was too short ({ttl.TotalMilliseconds}ms) and it has been unlocked immediately. Try using longer TTL value.");
 
             return false;
         }
 
-        private async Task<bool> LockInstance(string resource, TimeSpan ttl)
+        private async Task<bool> LockInstanceAsync(string resource, TimeSpan ttl)
         {
-            if (connection.IsConnected == false)
+            return await ExecuteAsync(async connection =>
             {
-                var message = $"Unreachable endpoint '{connection.ClientName}'. Unable to acquire lock on this node.";
-                log.LogWarning(message);
+                DateTimeOffset now = DateTimeOffset.UtcNow;
+                var absExpiration = now.Add(ttl).ToFileTime();
 
-                return false;
-            }
+                var prevAbsExpiration = await connection.GetDatabase().StringSetAndGetAsync(resource, absExpiration, ttl, When.Always, CommandFlags.DemandMaster).ConfigureAwait(false);
+                if (prevAbsExpiration.HasValue)
+                {
+                    var nextAbsExpiration = DateTimeOffset.FromFileTime((long)prevAbsExpiration);
+                    var newTTl = nextAbsExpiration - now;
+                    if (newTTl.TotalMilliseconds > 0)
+                    {
+                        await connection.GetDatabase().KeyExpireAsync(resource, newTTl, CommandFlags.FireAndForget).ConfigureAwait(false);
+                    }
+                }
 
-            try
-            {
-                return await connection.GetDatabase().StringSetAsync(resource, Guid.NewGuid().ToByteArray(), ttl, When.NotExists, CommandFlags.DemandMaster).ConfigureAwait(false);
-            }
-            catch (Exception ex)
-            {
-                var message = $"Unreachable endpoint '{connection.ClientName}'. Unable to acquire lock on this node.";
-                log.LogWarning(ex, message);
-
-                return false;
-            }
+                return prevAbsExpiration.HasValue == false;
+            });
         }
 
-        private async Task UnlockInstance(string resource)
+        private async Task UnlockInstanceAsync(string resource)
         {
-            if (connection.IsConnected == false)
-            {
-                log.LogWarning($"Unreachable endpoint '{connection.ClientName}'. Unable to unlock resource '{resource}'.");
-
-                return;
-            }
-
-            RedisKey[] key = { resource };
-
-            try
-            {
-                await connection.GetDatabase().KeyDeleteAsync(key, CommandFlags.DemandMaster).ConfigureAwait(false);
-            }
-            catch (Exception ex)
-            {
-                log.LogWarning(ex, $"Unreachable endpoint '{connection.ClientName}'. Unable to unlock resource '{resource}'.");
-            }
+            await ExecuteAsync(async connection =>
+                await connection.GetDatabase().KeyDeleteAsync(resource, CommandFlags.DemandMaster | CommandFlags.FireAndForget).ConfigureAwait(false)).ConfigureAwait(false);
         }
 
-        private async Task<bool> RetryAsync(Func<Task<bool>> action, int retryCount, TimeSpan retryDelay)
+        private async Task<bool> RetryAsync(Func<Task<bool>> action, ushort retryCount, TimeSpan retryDelay)
         {
             var currentRetry = 0;
             var result = false;
 
             while (currentRetry++ < retryCount)
             {
-                result = await action().ConfigureAwait(false);
-                if (result) break;
+                try
+                {
+                    result = await action().ConfigureAwait(false);
+                    if (result) break;
 
-                await Task.Delay(retryDelay).ConfigureAwait(false);
+                    await Task.Delay(retryDelay).ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Redlock operation has failed.");
+
+                    await Task.Delay(retryDelay).ConfigureAwait(false);
+                }
             }
 
             return result;
+        }
+
+        private async Task<T> ExecuteAsync<T>(Func<ConnectionMultiplexer, Task<T>> theLogic)
+        {
+            if (connectionDoNotUse is null || (connectionDoNotUse.IsConnected == false && connectionDoNotUse.IsConnecting == false))
+            {
+                try
+                {
+                    var configurationOptions = ConfigurationOptions.Parse(options.ConnectionString);
+                    connectionDoNotUse = await ConnectionMultiplexer.ConnectAsync(configurationOptions);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, $"Unable to establish connection with Redis: {options.ConnectionString}");
+                    throw;
+                }
+            }
+
+            try
+            {
+                return await theLogic(connectionDoNotUse).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"Unable to execute Redis query.");
+
+                return default;
+            }
         }
     }
 }
